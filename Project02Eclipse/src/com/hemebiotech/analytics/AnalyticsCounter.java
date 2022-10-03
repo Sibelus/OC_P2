49fafa01dc35de1021e8b52b6b3f661afbc53604
Gaëtan <ggaetan@proton.me>
2022-10-03 14:15:34 +0200

    Gestion des exceptions dans AnalyticsCounter.java

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,46 +3,56 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
+	//Initiate symptoms counter
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		//int i = 0;	// set i to 0
-		//int headCount = 0;	// counts headaches
-		while (line != null) {
-			//i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				//Alex used wrong variable 
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
+	public static void main(String args[]){
+		try {
+			//Read data from source file .txt
+			BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+			String line = reader.readLine();
+	
+			//Identify and increment symptoms counter
+			while (line != null) {				
+				switch(line) {
+					case "headache":
+						headacheCount++;
+						System.out.println("number of headaches: " + headacheCount);
+						break;
+					case "rash":
+						rashCount++;
+						System.out.println("number of headaches: " + rashCount);
+						break;
+					case "dialated pupils":
+						pupilCount++;
+						System.out.println("number of headaches: " + pupilCount);
+						break;
+					default:
+						System.out.println("symptom from file: " + line);
+				}
+				
+				// get another symptom
+				line = reader.readLine();
 			}
-			//Alex used wrong string to compare with ("rush" instead of ""rash")
-			else if (line.equals("rash")) {
-				rashCount++;
-				System.out.println("number of headaches: " + rashCount);
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-				System.out.println("number of headaches: " + pupilCount);
-			}
-
-			line = reader.readLine();	// get another symptom
+			reader.close();
+		}catch (IOException e) {
+			e.printStackTrace();
 		}
-		reader.close();
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		try {
+			//Write symptoms count on result.out file
+			FileWriter writer = new FileWriter ("result.out");
+			writer.write("headache: " + headacheCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("dialated pupils: " + pupilCount + "\n");
+			writer.close();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
